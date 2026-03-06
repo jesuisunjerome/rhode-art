182a0395ea2a083a23170fd91c480eb606d39d52
jesuisunjerome <rhodeexy@gmail.com>
2026-03-06 14:29:57 -0600

    feat: add Mercado Pago service for payment preference creation and retrieval.

--- backend/src/services/mercadopago.service.js
+++ backend/src/services/mercadopago.service.js
@@ -13,9 +13,8 @@ const client = new MercadoPagoConfig({
  */
 export const getMPPayment = async (paymentId) => {
   const payment = new Payment(client);
-  console.log(payment);
+  console.log("response", await payment.get({ id: paymentId }));
   const response = await payment.get({ id: paymentId });
-  console.log(response);
   return response;
 };
 
